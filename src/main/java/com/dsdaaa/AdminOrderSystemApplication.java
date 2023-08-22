20d6be4a095165c5a10800d44e9d9e9bd777fb4f
DSDAAA <129018686+DSDAAA@users.noreply.github.com>
2023-08-22 18:39:39 +0800

    初始化项目部署

---
+++ src/main/java/com/dsdaaa/AdminOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dsdaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsdaaa/AdminOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsdaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
